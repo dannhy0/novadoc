6b55eacf929ea90a24e2e173b053d5a960d6696b
dannhy0 <dannhym@gmail.com>
2022-09-27 12:51:59 +0200

    sans conf

--- conf.py
+++ conf.py
@@ -1,33 +1,33 @@
 
-extensions = [
-    '...' ,
-    'myst_parser',
+# extensions = [
+#     '...' ,
+#     'myst_parser',
     
-]
-
-
-source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
-
-# source_suffix = {
-
-#     '.rst': 'restructuredtext',
-#     '.txt': 'markdown',
-#     '.md': 'markdown',
-# }
-
-# myst_enable_extensions = [
-#     "amsmath",
-#     "colon_fence",
-#     "deflist",
-#     "dollarmath",
-#     "fieldlist",
-#     "html_admonition",
-#     "html_image",
-#     "linkify",
-#     "replacements",
-#     "smartquotes",
-#     "strikethrough",
-#     "substitution",
-#     "tasklist",
 # ]
+
+
+# source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+
+# # source_suffix = {
+
+# #     '.rst': 'restructuredtext',
+# #     '.txt': 'markdown',
+# #     '.md': 'markdown',
+# # }
+
+# # myst_enable_extensions = [
+# #     "amsmath",
+# #     "colon_fence",
+# #     "deflist",
+# #     "dollarmath",
+# #     "fieldlist",
+# #     "html_admonition",
+# #     "html_image",
+# #     "linkify",
+# #     "replacements",
+# #     "smartquotes",
+# #     "strikethrough",
+# #     "substitution",
+# #     "tasklist",
+# # ]
 
