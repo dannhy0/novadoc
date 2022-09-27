7719cf8cad8d1219b8f6be2b359708eebe6bdb2a
dannhy0 <dannhym@gmail.com>
2022-09-27 12:02:21 +0200

    ajout md

---
+++ conf.py
@@ -0,0 +1,33 @@
+project: Novadoc
+
+site_name: Novalend
+
+author: Dannhy
+
+extensions = ['myst_parser']
+
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+
+source_suffix = {
+
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
--- docs/usage.md
+++ docs/usage.md
@@ -1,3 +1,10 @@
-# Usage
+### Usage
 
 Voici un test sur le USAGE
+
+### 1. [Go to Novalend](http://front2:8742/index.php)
+
+-----
+
+### 2. Click on  Créer un devis
+![Step 2 screenshot](https://images.tango.us/public/screenshot_8315f0ad-cfb4-4274-b77e-d289fe09cad8.png?crop=focalpoint&fit=crop&fp-x=0.5664&fp-y=0.0327&fp-z=2.5610&w=1200&mark-w=0.2&mark-pad=0&mark64=aHR0cHM6Ly9pbWFnZXMudGFuZ28udXMvc3RhdGljL21hZGUtd2l0aC10YW5nby13YXRlcm1hcmsucG5n&ar=1846%3A980)
